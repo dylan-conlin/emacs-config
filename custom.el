203b585fc282e19d22661c1a4f6795ff4ca07072
Dylan Conlin <dylan.conlin@gmail.com>
2015-11-10 16:45:42 -0800

    added jsx stuff

--- custom.el
+++ custom.el
@@ -33,12 +33,34 @@
  '(cursor-in-non-selected-windows t)
  '(custom-safe-themes
    (quote
-    ("5d8caed7f4ed8929fd79e863de3a38fbb1aaa222970b551edfd2e84552fec020" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a5beb9b1d6dc23dd8a3c204c159c9a5f1e0115ff14b5b8579d6f3ede4f3b3aee" "28590b1f3a827a16ec1f025b20c025a6e63b56b6c4223851c224c2e6cd1c1775" "5ea20171762b3f9682fbf507ee4b4018ce7b6cc65415fa99799a125f112b2cdb" "b21bf64c01dc3a34bc56fff9310d2382aa47ba6bc3e0f4a7f5af857cd03a7ef7" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "1011be33e9843afd22d8d26b031fbbb59036b1ce537d0b250347c19e1bd959d0" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "609a1521b4983dc1e1cc64fe57fe2fffd256e9058c95b8fc6bfae479ab8b0af3" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+    ("beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "d9db2602073498bfa3d591e2ce70de3e9c144c30aeacf9e667b0fb9139f38f50" "5d8caed7f4ed8929fd79e863de3a38fbb1aaa222970b551edfd2e84552fec020" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a5beb9b1d6dc23dd8a3c204c159c9a5f1e0115ff14b5b8579d6f3ede4f3b3aee" "28590b1f3a827a16ec1f025b20c025a6e63b56b6c4223851c224c2e6cd1c1775" "5ea20171762b3f9682fbf507ee4b4018ce7b6cc65415fa99799a125f112b2cdb" "b21bf64c01dc3a34bc56fff9310d2382aa47ba6bc3e0f4a7f5af857cd03a7ef7" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "1011be33e9843afd22d8d26b031fbbb59036b1ce537d0b250347c19e1bd959d0" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "609a1521b4983dc1e1cc64fe57fe2fffd256e9058c95b8fc6bfae479ab8b0af3" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+ '(diary-entry-marker (quote font-lock-variable-name-face))
  '(dired-dwim-target t)
  '(emms-cache-get-function (quote emms-cache-get))
  '(emms-cache-modified-function (quote emms-cache-dirty))
  '(emms-cache-set-function (quote emms-cache-set))
  '(emms-mode-line-format " %s ")
+ '(emms-mode-line-icon-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *note[] = {
+/* width height num_colors chars_per_pixel */
+\"    10   11        2            1\",
+/* colors */
+\". c #1ba1a1\",
+\"# c None s None\",
+/* pixels */
+\"###...####\",
+\"###.#...##\",
+\"###.###...\",
+\"###.#####.\",
+\"###.#####.\",
+\"#...#####.\",
+\"....#####.\",
+\"#..######.\",
+\"#######...\",
+\"######....\",
+\"#######..#\" };")))
  '(emms-mode-line-mode-line-function (quote emms-mode-line-icon-function))
  '(emms-mode-line-titlebar-function (quote ignore))
  '(emms-playlist-default-major-mode (quote emms-playlist-mode))
@@ -49,10 +71,10 @@
  '(erc-track-position-in-mode-line t)
  '(eshell-cd-shows-directory nil)
  '(eshell-cmpl-autolist t)
- '(eshell-highlight-prompt t)
+ '(eshell-highlight-prompt t t)
  '(eshell-hist-ignoredups t)
  '(eshell-list-files-after-cd nil)
- '(eshell-prompt-regexp "^[^#$]*[#$] ")
+ '(eshell-prompt-regexp "^[^#$]*[#$] " t)
  '(eshell-scroll-to-bottom-on-input (quote all))
  '(eshell-scroll-to-bottom-on-output (quote all))
  '(fci-rule-color "#f1c40f")
@@ -63,6 +85,30 @@
  '(global-auto-complete-mode t)
  '(global-git-gutter-mode t)
  '(global-hl-line-mode nil)
+ '(gnus-logo-colors (quote ("#1ec1c4" "#bababa")) t)
+ '(gnus-mode-line-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *gnus-pointer[] = {
+/* width height num_colors chars_per_pixel */
+\"    18    13        2            1\",
+/* colors */
+\". c #1ba1a1\",
+\"# c None s None\",
+/* pixels */
+\"##################\",
+\"######..##..######\",
+\"#####........#####\",
+\"#.##.##..##...####\",
+\"#...####.###...##.\",
+\"#..###.######.....\",
+\"#####.########...#\",
+\"###########.######\",
+\"####.###.#..######\",
+\"######..###.######\",
+\"###....####.######\",
+\"###..######.######\",
+\"###########.######\" };")) t)
  '(guide-key/align-command-by-space-flag t)
  '(guide-key/idle-delay 0)
  '(guide-key/recursive-key-sequence-flag t)
@@ -79,7 +125,7 @@
    (quote
     ("\\` " "\\*helm" "\\*helm-mode" "\\*Echo Area" "\\*Minibuf" "\\*project(less)-search" "\\*project-search" "\\*magit-process")))
  '(helm-buffer-details-flag t)
- '(helm-buffer-max-length 30)
+ '(helm-buffer-max-length 60)
  '(helm-completing-read-handlers-alist
    (quote
     ((describe-function . helm-completing-read-symbols)
@@ -92,6 +138,7 @@
      (nil))))
  '(helm-ff-transformer-show-only-basename nil)
  '(helm-ls-git-show-abs-or-relative (quote relative))
+ '(helm-persistent-action-use-special-display nil)
  '(helm-quick-update t)
  '(helm-reuse-last-window-split-state t)
  '(helm-swoop-pre-input-function
@@ -108,7 +155,7 @@
  (solarized-color-blend it "#fdf6e3" 0.25)
  (quote
   ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
-'(highlight-symbol-foreground-color "#586e75")
+ '(highlight-symbol-foreground-color "#586e75")
 '(highlight-tail-colors
 (quote
  (("#eee8d5" . 0)
@@ -125,61 +172,62 @@
 '(hl-fg-colors
 (quote
  ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
-'(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
-'(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
-'(ido-ubiquitous-fallback-completing-read-function (quote completing-read-default))
-'(isearch-lazy-highlight t)
-'(ispell-highlight-face (quote flyspell-incorrect))
-'(js2-missing-semi-one-line-override t)
-'(js2-strict-missing-semi-warning nil)
-'(lazy-highlight-initial-delay 0)
-'(linum-format " %7d ")
-'(magit-completing-read-function (quote magit-builtin-completing-read))
-'(magit-diff-use-overlays nil)
-'(magit-item-highlight-face (quote magit-item-highlight))
-'(magit-use-overlays nil)
+ '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
+ '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
+ '(ido-ubiquitous-fallback-completing-read-function (quote completing-read-default))
+ '(isearch-lazy-highlight t)
+ '(ispell-highlight-face (quote flyspell-incorrect))
+ '(js2-missing-semi-one-line-override t)
+ '(js2-strict-missing-semi-warning nil)
+ '(lazy-highlight-initial-delay 0)
+ '(linum-format " %7d ")
+ '(magit-completing-read-function (quote magit-builtin-completing-read))
+ '(magit-diff-use-overlays nil)
+ '(magit-item-highlight-face (quote magit-item-highlight))
+ '(magit-use-overlays nil)
 '(newsticker-url-list
 (quote
  (("dissolve" "http://thedissolve.com/feeds/reviews/essential/" nil nil nil))))
 '(org-agenda-files
 (quote
  ("~/Dropbox/org/code.org" "~/Dropbox/org/notes.org" "~/Dropbox/org/shortstack.org" "~/Dropbox/org/todo.org")))
-'(org-default-notes-file "~/Dropbox/org/notes.org")
-'(org-directory "~/Dropbox/org/")
-'(org-from-is-user-regexp "\\<Dylan Conlin\\>")
-'(org-journal-dir "~/Dropbox/org/journal/")
-'(org-log-done t)
-'(org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-'(org-mobile-inbox-for-pull "~/Dropbox/org/mobile.org")
-'(org-startup-indented t)
-'(package-build-verbose nil)
-'(paradox-automatically-star nil)
-'(paradox-github-token "9935f1b73d570a20fc348650741f836587963731")
-'(projectile-completion-system (quote helm))
-'(projectile-enable-caching t)
-'(projectile-indexing-method (quote alien))
-'(recentf-max-menu-items 100)
-'(recentf-max-saved-items 100)
-'(safe-local-variable-values (quote ((encoding . utf-8) (lexical-binding . t))))
-'(scss-compile-at-save nil)
-'(sentence-end-double-space nil)
-'(show-paren-delay 0.06)
-'(show-smartparens-global-mode t)
-'(show-trailing-whitespace nil)
-'(slack-token (getenv "SLACK_TOKEN"))
-'(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
-'(sml/active-background-color "#34495e")
-'(sml/active-foreground-color "#ecf0f1")
-'(sml/inactive-background-color "#dfe4ea")
-'(sml/inactive-foreground-color "#34495e")
-'(sml/replacer-regexp-list nil)
-'(sp-hybrid-kill-excessive-whitespace t)
-'(sp-show-pair-delay 0)
-'(sp-show-pair-from-inside t)
-'(sp-undo-pairs-separately t)
-'(term-default-bg-color "#fdf6e3")
-'(term-default-fg-color "#657b83")
-'(vc-annotate-background "#ecf0f1")
+ '(org-default-notes-file "~/Dropbox/org/notes.org")
+ '(org-directory "~/Dropbox/org/")
+ '(org-from-is-user-regexp "\\<Dylan Conlin\\>")
+ '(org-journal-dir "~/Dropbox/org/journal/")
+ '(org-log-done t)
+ '(org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+ '(org-mobile-inbox-for-pull "~/Dropbox/org/mobile.org")
+ '(org-startup-indented t)
+ '(package-build-verbose nil)
+ '(paradox-automatically-star nil)
+ '(paradox-github-token "9935f1b73d570a20fc348650741f836587963731")
+ '(projectile-completion-system (quote helm))
+ '(projectile-enable-caching t)
+ '(projectile-indexing-method (quote alien))
+ '(recentf-max-menu-items 100)
+ '(recentf-max-saved-items 100)
+ '(red "#ffffff")
+ '(safe-local-variable-values (quote ((encoding . utf-8) (lexical-binding . t))))
+ '(scss-compile-at-save nil)
+ '(sentence-end-double-space nil)
+ '(show-paren-delay 0.06)
+ '(show-smartparens-global-mode t)
+ '(show-trailing-whitespace nil)
+ '(slack-token (getenv "SLACK_TOKEN"))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(sml/active-background-color "#34495e")
+ '(sml/active-foreground-color "#ecf0f1")
+ '(sml/inactive-background-color "#dfe4ea")
+ '(sml/inactive-foreground-color "#34495e")
+ '(sml/replacer-regexp-list nil)
+ '(sp-hybrid-kill-excessive-whitespace t)
+ '(sp-show-pair-delay 0)
+ '(sp-show-pair-from-inside t)
+ '(sp-undo-pairs-separately t)
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background "#ecf0f1")
 '(vc-annotate-color-map
 (quote
  ((30 . "#e74c3c")
@@ -194,16 +242,16 @@
   (300 . "#16a085")
   (330 . "#2492db")
   (360 . "#0a74b9"))))
-'(vc-annotate-very-old-color "#0a74b9")
-'(web-mode-attr-indent-offset nil)
-'(web-mode-code-indent-offset 2)
-'(web-mode-css-indent-offset 2)
-'(web-mode-markup-indent-offset 2)
+ '(vc-annotate-very-old-color "#0a74b9")
+ '(web-mode-attr-indent-offset nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2)
 '(weechat-color-list
 (quote
  (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
-'(whitespace-line-column 300)
-'(win-switch-feedback-background-color "green"))
+ '(whitespace-line-column 300)
+ '(win-switch-feedback-background-color "green"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init.el
+++ init.el
@@ -178,7 +178,7 @@
 
 (defun my/setup-light-theme ()
   (interactive)
-  (load-theme 'base16-google-light t)
+  (load-theme 'apropospriate-light t)
   ;; only show bad whitespace
   (setq whitespace-style '(face trailing tab lines newline empty space-before-tab indentation empty space-after-tab))
   (setq dired-details-propagate-flag t)
@@ -660,5 +660,28 @@
 
 ;; (remove-hook 'before-save-hook 'cleanup-buffer)
 
-;; (global-aggressive-indent-mode)
+(recentf-mode 1)
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker)
+              (flycheck-mode))))
+(setq jsx-indent-level 2)
+
 (open-last-visited-file)
